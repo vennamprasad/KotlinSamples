ab0a0ab08a01038b5a9c83f0981e37e1817cf664
vennam prasad <vennamprasad@gmail.com>
2019-04-29 19:26:18 +0530

    network and package

--- app/src/main/java/com/example/myapplication/activities/AboutUs.kt
+++ app/src/main/java/com/example/myapplication/activities/AboutUs.kt
@@ -1,25 +1,36 @@
 package com.example.myapplication.activities
 
-import android.app.Dialog
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.example.myapplication.intro.MApplication
 import com.example.myapplication.intro.NetworkCheck
 
 class AboutUs : AppCompatActivity(), NetworkCheck.ConnectivityReceiverListener {
-    var no_internet_dialog: Dialog? = null
-
+    private var webView: WebView? = null
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_us)
-        no_internet_dialog = Dialog(this@AboutUs)
-        no_internet_dialog!!.setContentView(R.layout.content_network_detect)
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView!!.canGoBack()) {
+            webView!!.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
 
     override fun onStart() {
         super.onStart()
-        MApplication.getInstance().setConnectivityListener(this@AboutUs)
+        MApplication.getInstance().setConnectivityListener(this)
     }
 
     override fun onPause() {
@@ -29,10 +40,31 @@ class AboutUs : AppCompatActivity(), NetworkCheck.ConnectivityReceiverListener {
 
     override fun onNetworkConnectionChanged(isConnected: Boolean) {
         if (isConnected) {
-            no_internet_dialog?.dismiss()
+            reload()
         } else {
-            no_internet_dialog?.setCancelable(false)
-            no_internet_dialog?.show()
+            this.setContentView(R.layout.content_network_detect)
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun reload() {
+        setContentView(R.layout.activity_about_us)
+        webView = findViewById(R.id.web)
+        val webSettings = webView!!.settings
+        webSettings.javaScriptEnabled = true
+        webView!!.webViewClient = HelloWebViewClient()
+        webView!!.loadUrl("https://stackoverflow.com/legal/acceptable-use-policy")
+    }
+
+    private inner class HelloWebViewClient : WebViewClient() {
+
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            if (Uri.parse(url).host == "https://stackoverflow.com") {
+                return false
+            }
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+            return true
         }
     }
 }
--- app/src/main/java/com/example/myapplication/frags/Play.kt
+++ app/src/main/java/com/example/myapplication/frags/Play.kt
@@ -9,8 +9,7 @@ import com.example.myapplication.R
 
 class Play : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_play, container, false)
     }
 
